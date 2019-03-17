31aac4373ac9ffae53be8a485ad3a8fac481891d
Zafer Khourdaji <khourdaji@gmail.com>
2019-03-17 15:18:39 -0400

    Minor refactor Insertion Sort

--- Algorithms/InsertionSort.js
+++ Algorithms/InsertionSort.js
@@ -5,12 +5,9 @@
  */
 function insertionSort(array){
     for (let i = 1; i < array.length; i++){
-       for (let j = i; j > 0; j--){
+       for (let j = i; j >= 0 && array[j-1] > array[j]; j--){
            if (array[j-1] > array[j]){
                swap(array, j-1, j);
-           }
-           else{
-               break;
            }
        }
     }
