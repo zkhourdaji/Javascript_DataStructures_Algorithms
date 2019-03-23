b16d668a0131d8e259f69bc928bd4c765f900d66
Zafer Khourdaji <khourdaji@gmail.com>
2019-03-23 15:22:00 -0400

    refactor merge method

--- Algorithms/MergeSort.js
+++ Algorithms/MergeSort.js
@@ -3,23 +3,23 @@ function merge(array1, array2){
     let i = 0;
     let j = 0;
 
-    while (i < array1.length || j < array2.length){
-        
-        if (array1[i] < array2[j] || array1[i] && !array2[j]){
+    while (i < array1.length && j < array2.length){
+        if (array1[i] < array2[j]){
             result.push(array1[i]);
             i++;
-        }
-        else if (array2[j] < array1[i] || array2[j] && !array1[i]){
-            result.push(array2[j]);
-            j++;
         }
         else {
-            result.push(array1[i]);
             result.push(array2[j]);
-            i++;
             j++;
         }
     }
+    // this happens if one array is bigger than the other
+    // we will exit the first while loop but still have elements from
+    // one array to add to results
+    for (i; i < array1.length; i++)
+        result.push(array1[i]);
+    for (j; j < array2.length; j++)
+        result.push(array2[j]);
     return result;
 }
 
